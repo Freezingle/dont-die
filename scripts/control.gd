6e34e458fbd1c740eba12522ab0be60c74c80972
Anmol <078bct011@student.ioepc.edu.np>
2026-06-02 14:14:31 +0545

    Add announcement timers and ui update

---
+++ scripts/control.gd
@@ -0,0 +1,7 @@
+extends Control 
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func _on_about_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/ui/AboutMenu.tscn")	
