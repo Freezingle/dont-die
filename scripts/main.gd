256c698ee476d11d03c23b3482e06fed0d66adf3
Anmol <078bct011@student.ioepc.edu.np>
2026-06-02 14:14:52 +0545

    add logo and implement game over loop

--- scripts/main.gd
+++ scripts/main.gd
@@ -20,12 +20,15 @@ var bomb_phase
 
 @onready var game_over_ui = $UI/GameOverUI
 @onready var restart_button = $UI/GameOverUI/RestartButton
+@onready var announcement_label = $UI/RootUI/AnnouncementLabel
 @onready var difficulty_timer = $DifficultyTimer
-@onready var level_label = $UI/RootUI/LevelLabel
 @onready var timer_label = $UI/RootUI/TimerLabel
 @onready var cannon_spawner = $World/CannonSpawner
 @onready var level_timer = $LevelTimer
 @onready var cannon_spawnner = $World/CannonSpawner
+@onready var victory_ui = $UI/VictoryUI
+@onready var victory_main_menu_button = $UI/VictoryUI/MainMenu
+@onready var victory_label = $UI/VictoryUI/Label
 
 func _process (delta):
 	if current_phase != GamePhase.LEVEL_2:
@@ -43,12 +46,9 @@ func _ready():
 	level_timer.timeout.connect(_on_level_timer_timeout)
 	if player:
 		player.died.connect(on_player_died)
-
-	restart_button.pressed.connect(restart_game)
 	difficulty_timer.timeout.connect(increase_difficulty)
-	level_label.text = "LEVEL 1"
-	update_timer_ui()
-
+	
+	restart_button.pressed.connect(restart_game)
 
 func increase_score():
 	if game_over:
@@ -62,16 +62,30 @@ func increase_difficulty():
 
 # game clock
 func _on_level_timer_timeout():
-	elapsed_time +=1
-	update_timer_ui()
+	elapsed_time += 1
+
+	# LEVEL 2 WARNING
+	if current_phase == GamePhase.LEVEL_1:
+		var remaining = 60 - elapsed_time
+
+		if remaining <= 5 and remaining > 0:
+			show_announcement("WARNING\nCENTER HATCH OPENING\n" + str(remaining))
+
+		if remaining == 0:
+			hide_announcement()
+
+	# FINAL LEVEL WARNING
+	if current_phase == GamePhase.LEVEL_2:
+		var remaining = 100 - elapsed_time
+
+		if remaining <= 5 and remaining > 0:
+			show_announcement("WARNING\nEXPLOSIONS IN\n" + str(remaining))
+
+		if remaining == 0:
+			hide_announcement()
+
 	check_phase_progression()
 
-func update_timer_ui():
-	match current_phase:
-		GamePhase.LEVEL_1:
-			timer_label.text = "NEXT LEVEL: " + str(max(0,60-elapsed_time))
-		GamePhase.LEVEL_2:
-			timer_label.text = "FINAL LEVEL: " + str(max(0,90-elapsed_time))
 
 func check_phase_progression():
 	if current_phase == GamePhase.LEVEL_1 and elapsed_time >=60:
@@ -82,14 +96,12 @@ func check_phase_progression():
 		
 func start_level_2():
 	current_phase = GamePhase.LEVEL_2
-	level_label.text = "LEVEL 2"
 	difficulty_timer.stop()
 	spawn_pit()
 	print("LEVEL 2 STARTED")
 
 func start_final_level():
 	current_phase = GamePhase.FINAL_LEVEL
-	level_label.text = "FINAL LEVEL"
 	# STOP CANNONS
 	if cannon_spawner:
 		cannon_spawner.queue_free()
@@ -128,8 +140,23 @@ func spawn_bomb_phase():
 	bomb_phase.phase_completed.connect(_on_bomb_phase_completed)
 	
 func _on_bomb_phase_completed():
-	print("YOU WIN")
+	game_over = true
+
+	victory_label.text = "YOU SURVIVED!GAME OVER!\n Thanks for playing!"
+	victory_ui.visible = true
+	get_tree().paused = true
 
 func restart_game():
 	get_tree().paused = false
 	get_tree().reload_current_scene()
+	
+func show_announcement(text:String):
+	announcement_label.visible = true
+	announcement_label.text = text
+
+func hide_announcement():
+	announcement_label.visible = false
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/ui/MainMenu.tscn")
